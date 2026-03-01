b5ca341ed83f6d493f0df0245e4e23ee4f8a46a9
achrafaitmbarek <63782686+achrafaitmbarek@users.noreply.github.com>
2026-03-01 05:04:18 +0100

    AUTH-07 : add Swagger OpenAPI documentation

--- src/main/java/com/bank/authservice/config/SecurityConfig.java
+++ src/main/java/com/bank/authservice/config/SecurityConfig.java
@@ -28,6 +28,11 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
--- src/main/java/com/bank/authservice/controller/UserController.java
+++ src/main/java/com/bank/authservice/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.bank.authservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +12,14 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "User", description = "Protected user endpoints")
+@SecurityRequirement(name = "Bearer Authentication")
+
 public class UserController {
 
+
     @GetMapping("/me")
+    @Operation(summary = "Get current authenticated user")
     public ResponseEntity<Map<String, String>> getCurrentUser() {
 
         // SecurityContextHolder contient ce que notre filtre a mis
@@ -19,7 +27,7 @@ public class UserController {
         String email = SecurityContextHolder
                 .getContext()
                 .getAuthentication()
-                .getName();  // getName() retourne le principal = l'email
+                .getName();
 
         return ResponseEntity.ok(Map.of(
                 "email", email,
